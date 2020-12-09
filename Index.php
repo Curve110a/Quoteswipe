9f6711e64d47fbfcc76f50a59fb77a765f448097
Curve110a <Rick110a@hotmail.com>
2020-12-09 15:20:32 +0100

    quoteswipe first commit

---
+++ Index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>QuoteSwipe</title>
+</head>
+
+<body>
+    <main>
+    <div class="title">Quote<span class="titleRed">SWIPE</span></div>
+    </main>
+</body>
+
+</html>
